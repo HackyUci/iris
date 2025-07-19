d8cdf2bbd56ae816394f35db81d2d5560d0d5ff9
Grace Karina <gracekarina12@gmail.com>
2025-07-19 18:44:00 +0700

    feat: add exchange_service module and update exports in mod.rs

--- src/iris_backend/src/services/mod.rs
+++ src/iris_backend/src/services/mod.rs
@@ -1,7 +1,9 @@
 pub mod bitcoin_service;
 pub mod qr_service;
 pub mod invoice_service;
+pub mod exchange_service;
 
 pub use bitcoin_service::*;
 pub use qr_service::*;
 pub use invoice_service::*;
+pub use exchange_service::*;
