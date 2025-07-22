ebae69df559b376f77924c31ade8533a3680d27b
Grace Karina <gracekarina12@gmail.com>
2025-07-22 12:22:12 +0700

    feat: create enums module for user roles, currencies, payment statuses, and methods

---
+++ src/iris_backend/src/models/enums/mod.rs
@@ -0,0 +1,13 @@
+pub mod user_role;
+pub mod currency;
+pub mod payment_status;
+pub mod payment_method;
+pub mod invoice_status;
+pub mod cashout_status;
+
+pub use user_role::*;
+pub use currency::*;
+pub use payment_status::*;
+pub use payment_method::*;
+pub use invoice_status::*;
+pub use cashout_status::*;
